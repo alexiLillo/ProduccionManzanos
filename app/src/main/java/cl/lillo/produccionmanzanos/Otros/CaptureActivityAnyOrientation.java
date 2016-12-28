2a30c50f8196f7320dd7ca094d5bf55b82602b10
Alexi <alexi.lillo92@gmail.com>
2016-12-28 13:15:19 -0300

    Initial commit

---
+++ app/src/main/java/cl/lillo/produccionmanzanos/Otros/CaptureActivityAnyOrientation.java
@@ -0,0 +1,10 @@
+package cl.lillo.produccionmanzanos.Otros;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * Created by root on 6/7/16.
+ */
+public class CaptureActivityAnyOrientation extends CaptureActivity {
+
+}
